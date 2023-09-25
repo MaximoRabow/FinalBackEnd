418d473151969e017a6d9e4cf74edba4498cb511
MaximoRabow <113936807+MaximoRabow@users.noreply.github.com>
2023-09-25 17:30:06 -0300

    creacion userSchema

--- src/users/users.dao.js
+++ src/users/users.dao.js
@@ -2,3 +2,24 @@ import mongoose from "mongoose";
 
 const collection = "users";
 
+const userSchema = new mongoose.Schema({
+    name: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    password: {
+        type: String,
+        select: false,
+        required: true,
+    }
+})
+
+const Users = mongoose.model(collection, userSchema);
+
+export default Users;
+
