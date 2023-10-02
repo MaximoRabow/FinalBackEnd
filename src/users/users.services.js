1e21c9a4adfe90882f833c45949991c915b06238
Maximo Rabow <mrabow@proguidemc.com>
2023-10-02 12:25:48 -0300

    repository service

---
+++ src/users/users.services.js
@@ -0,0 +1,7 @@
+import { config } from "../config/enviroment.config.js";
+import UserRepository from "./users.repository.js";
+
+
+const userService = new UserRepository(config.userDAO);
+
+export default userService;
